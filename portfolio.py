62204eee407d56593bbf98b27265c27dccb2a854
GuilhermSants <guisant1003@gmail.com>
2025-01-19 14:38:24 -0300

    portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -58,6 +58,26 @@ def main():
     except Exception as e:
         st.sidebar.error("Erro ao carregar a imagem de fundo do menu.")
 
+    # Adiciona a imagem de fundo da tela principal
+    try:
+        bg_image_base64_main = imagem_para_base64("fotoprincipal.jpg")
+        if bg_image_base64_main:
+            st.markdown(
+                f"""
+                <style>
+                body {{
+                    background-image: url("data:image/jpeg;base64,{bg_image_base64_main}");
+                    background-size: cover;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+    except Exception as e:
+        st.error("Erro ao carregar a imagem de fundo da tela principal.")
+
     # Exibe a imagem uma vez, na parte superior da barra lateral
     img = recorte_imagem_redonda("portfolio.jpg")  # Substitua com o caminho correto da imagem
     if img:
@@ -84,7 +104,7 @@ def main():
 # Função para exibir os dados pessoais
 def exibir_dados_pessoais():
     st.title("Dados Pessoais")
-    st.write("""
+    st.write("""  
     **Guilherme Henrique Damas dos Santos**  
     Idade: 24 anos  
     Nacionalidade: Brasileiro  
@@ -102,7 +122,7 @@ def exibir_curriculo():
 
     # Resumo das qualificações
     st.header("Resumo das Qualificações")
-    st.write("""
+    st.write("""  
     **Engenheiro Civil** com forte experiência na execução e gerenciamento de projetos estruturais. 
     Especializado na utilização de ferramentas de ponta como **Revit**, **Dynamo para Revit** e **Python**, 
     que permitem otimizar o desenvolvimento de projetos e automação de processos construtivos. 
@@ -115,7 +135,7 @@ def exibir_curriculo():
 # Função para exibir o objetivo
 def exibir_objetivo():
     st.title("Objetivo Profissional")
-    st.write("""
+    st.write("""  
     Buscar novas oportunidades de **desenvolvimento e implementação de soluções tecnológicas** no setor de **engenharia civil**, 
     com foco em **automação de processos** e **inovação**. Meu objetivo é liderar projetos que integrem as tecnologias de **Revit**, **Dynamo** e **Python** 
     para otimizar a **modelagem de informações de construção (BIM)**, possibilitando a criação de **projetos sustentáveis**, 
@@ -125,36 +145,36 @@ def exibir_objetivo():
 # Função para exibir a formação
 def exibir_formacao():
     st.title("Formação Acadêmica")
-    st.write("""
-    - **Dynamo para Revit** – **NeuroBIM** (Término previsto Fev/2025) :star: *Especialização em automação de fluxos de trabalho no Revit com Dynamo.*
-    - **Programação em Python** – **SENAI** (2024) :star: *Curso de introdução e base ao desenvolvimento das linhas de códigos e interpretação.*
-    - **Pós-Graduação** em **Gestão de Projetos Sustentáveis de Edificações** – **Faculdade Alphaville** (2023)
-    - **Graduação** em **Engenharia Civil** – **Universidade Paulista (UNIP)** (2022)
+    st.write("""  
+    - **Dynamo para Revit** – **NeuroBIM** (Término previsto Fev/2025) :star: *Especialização em automação de fluxos de trabalho no Revit com Dynamo.*  
+    - **Programação em Python** – **SENAI** (2024) :star: *Curso de introdução e base ao desenvolvimento das linhas de códigos e interpretação.*  
+    - **Pós-Graduação** em **Gestão de Projetos Sustentáveis de Edificações** – **Faculdade Alphaville** (2023)  
+    - **Graduação** em **Engenharia Civil** – **Universidade Paulista (UNIP)** (2022)  
     - **Curso de AutoCAD (2D/3D)** – **UNIARA QUALIFICA** (2021)
     """)
 
 # Função para exibir os conhecimentos
 def exibir_conhecimentos():
     st.title("Conhecimentos e Habilidades")
-    st.write("""
-    - **Revit**: Profundo domínio em **modelagem** e **planejamento** de projetos estruturais, com ênfase em **BIM** e **coordenação de projetos**.
-    - **Dynamo para Revit**: Criação de scripts para automação de tarefas, como **parametrização**, **otimização de processos** e **gerenciamento de dados** no Revit.
-    - **Python**: Desenvolvimento de **scripts** para automação de cálculos estruturais, integração de **dados** e controle de **fluxos de trabalho**.
-    - **AutoCAD / STRAP / Ftool**: Conhecimento avançado em **detalhamento estrutural** e **cálculos** com ferramentas especializadas.
-    - **Inventor**: Conhecimento básico na modelagem e usabilidade do software.
-    - **Produção em Campo**: Experiência prática em **acompanhamento de obras**, **execução de projetos** e **gestão de processos construtivos**.
-    - **Idiomas**: Inglês básico (habilidade para leitura e compreensão de textos).
+    st.write("""  
+    - **Revit**: Profundo domínio em **modelagem** e **planejamento** de projetos estruturais, com ênfase em **BIM** e **coordenação de projetos**.  
+    - **Dynamo para Revit**: Criação de scripts para automação de tarefas, como **parametrização**, **otimização de processos** e **gerenciamento de dados** no Revit.  
+    - **Python**: Desenvolvimento de **scripts** para automação de cálculos estruturais, integração de **dados** e controle de **fluxos de trabalho**.  
+    - **AutoCAD / STRAP / Ftool**: Conhecimento avançado em **detalhamento estrutural** e **cálculos** com ferramentas especializadas.  
+    - **Inventor**: Conhecimento básico na modelagem e usabilidade do software.  
+    - **Produção em Campo**: Experiência prática em **acompanhamento de obras**, **execução de projetos** e **gestão de processos construtivos**.  
+    - **Idiomas**: Inglês básico (habilidade para leitura e compreensão de textos).  
     """)
 
 # Função para exibir a experiência profissional
 def exibir_experiencia():
     st.title("Experiência Profissional")
-    st.markdown("""
+    st.markdown("""  
     **USICON CONSTRUÇÕES PRÉ-FABRICADAS LTDA** (04/2022 – Atual)  
     - **Cargo**: Projetista Pleno  
-      - Modelagem paramétrica no **Revit**, concepção e manutenção.
-      - Desenvolvimento de rotinas de automação para **Revit** utilizando o **Dynamo**.       
-      - Realização de detalhamento de peças pré-fabricadas com **AutoCAD**, **Revit** e planilhas **Excel**.
+      - Modelagem paramétrica no **Revit**, concepção e manutenção.  
+      - Desenvolvimento de rotinas de automação para **Revit** utilizando o **Dynamo**.  
+      - Realização de detalhamento de peças pré-fabricadas com **AutoCAD**, **Revit** e planilhas **Excel**.  
       - Realização de cálculos e verificações com **STRAP**, **Ftool** e **AutoCAD (AutoLISP)**.  
       - Acompanhamento de execução em campo, plotagem de projetos e solução de dúvidas de execução.  
 
